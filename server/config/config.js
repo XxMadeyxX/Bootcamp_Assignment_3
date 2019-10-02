c9b89455b687ef3c47f92dd28631017e32a216da
XxMadeyxX <32100184+XxMadeyxX@users.noreply.github.com>
2019-10-02 11:13:57 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    uri: 'mongodb+srv://zesty:PiggyBank@cen3031-cluster-xop6x.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
+  },
+  openCage: {
+    key: '0992ecbce4bb4d67bf79b1c608846671' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+  },
+  port: 8080
+};
